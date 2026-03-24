ce675fd63154fc00056ae326ee39bd3882df4d84
Siva Basani <sivabasani95@gmail.com>
2026-03-24 17:08:21 -0500

    update Home page

---
+++ BookMyScreen-Project/src/assets/components/Recommended.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Recommended = () => {
+    return (
+        <div className="recommended-container">
+            <div className="recommended-wrapper">
+                <div className="recommended-header">
+                    <h2 className="recommended-title">Recommended</h2>
+
+                    <span className="recommended-seeall">
+                        See All
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Recommended;
--- BookMyScreen-Project/src/pages/Home.jsx
+++ BookMyScreen-Project/src/pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     return (
         <div>
             <BannerSlider />
+            <Recommended />
         </div>
     );
 }
